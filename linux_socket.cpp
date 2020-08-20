8f61df29e9d6d72959435a7875fe2111b7b5e3b2
jinwei <jinweikim@gmail.com>
2020-08-20 13:12:45 +0800

    second commmit

--- linux_socket.cpp
+++ linux_socket.cpp
@@ -4,27 +4,35 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 int main(){
     //创建套接字
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    int serv_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-    //向服务器（特定的IP和端口）发起请求
+    //将套接字和IP、端口绑定
     struct sockaddr_in serv_addr;
     memset(&serv_addr, 0, sizeof(serv_addr));  //每个字节都用0填充
     serv_addr.sin_family = AF_INET;  //使用IPv4地址
     serv_addr.sin_addr.s_addr = inet_addr("169.254.105.143");  //具体的IP地址
-    serv_addr.sin_port = htons(8990);  //端口
-    connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-   
-    //读取服务器传回的数据
-    char buffer[40];
-    read(sock, buffer, sizeof(buffer)-1);
-   
-    printf("Message form server: %s\n", buffer);
+    serv_addr.sin_port = htons(1234);  //端口
+    bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+    //进入监听状态，等待用户发起请求
+    listen(serv_sock, 20);
+
+    //接收客户端请求
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_size = sizeof(clnt_addr);
+    int clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+
+    //向客户端发送数据
+    char str[] = "http://c.biancheng.net/socket/";
+    write(clnt_sock, str, sizeof(str));
    
     //关闭套接字
-    close(sock);
+    close(clnt_sock);
+    close(serv_sock);
 
     return 0;
 }
--- win_socket.cpp
+++ win_socket.cpp
@@ -1,41 +1,37 @@
 #include <stdio.h>
-#include <winsock2.h>
-#pragma comment (lib, "ws2_32.lib")  //加载 ws2_32.dll
+#include <stdlib.h>
+#include <WinSock2.h>
+#pragma comment(lib, "ws2_32.lib")  //加载 ws2_32.dll
 
 int main(){
-    //初始化 DLL
+    //初始化DLL
     WSADATA wsaData;
-    WSAStartup( MAKEWORD(2, 2), &wsaData);
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
 
     //创建套接字
-    SOCKET servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    SOCKET sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-    //绑定套接字
+    //向服务器发起请求
     sockaddr_in sockAddr;
     memset(&sockAddr, 0, sizeof(sockAddr));  //每个字节都用0填充
-    sockAddr.sin_family = PF_INET;  //使用IPv4地址
-    sockAddr.sin_addr.s_addr = inet_addr("169.254.224.108");  //具体的IP地址
-    sockAddr.sin_port = htons(8990);  //端口
-    bind(servSock, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR));
+    sockAddr.sin_family = PF_INET;
+    sockAddr.sin_addr.s_addr = inet_addr("169.254.224.108");
+    sockAddr.sin_port = htons(8990);
+    connect(sock, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR));
 
-    //进入监听状态
-    listen(servSock, 20);
+    //接收服务器传回的数据
+    char szBuffer[MAXBYTE] = {0};
+    recv(sock, szBuffer, MAXBYTE, NULL);
 
-    //接收客户端请求
-    SOCKADDR clntAddr;
-    int nSize = sizeof(SOCKADDR);
-    SOCKET clntSock = accept(servSock, (SOCKADDR*)&clntAddr, &nSize);
-
-    //向客户端发送数据
-    char *str = "Hello World!";
-    send(clntSock, str, strlen(str)+sizeof(char), NULL);
+    //输出接收到的数据
+    printf("Message form server: %s\n", szBuffer);
 
     //关闭套接字
-    closesocket(clntSock);
-    closesocket(servSock);
+    closesocket(sock);
 
-    //终止 DLL 的使用
+    //终止使用 DLL
     WSACleanup();
 
+    system("pause");
     return 0;
 }
