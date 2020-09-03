6d401d11da94ff04348dc1ced5dc82b01636c7b9
jinwei <jinweikim@gmail.com>
2020-09-03 14:35:42 +0800

    update usb_linux2.c

--- usb_linux2.c
+++ usb_linux2.c
@@ -4,36 +4,38 @@
 #include <fcntl.h>
 #include <asm/termios.h>
  
-#define DEV_NAME  "/dev/ttyS1"
+#define DEV_NAME  "/dev/ttyAMA0"
  
  
 int main (int argc, char *argv[])
 {
     int fd;
     int len, i,ret;
-        char buf[] = "hello ZLG!";
- 
+    char writeBuff[] = "we have received the message";
+    char readBuff[256];
+
  
     fd = open(DEV_NAME, O_RDWR | O_NOCTTY);
-        if(fd < 0) {
-                perror(DEV_NAME);
-                return -1;
-        }
- 
- 
-    len = write(fd, buf, sizeof(buf));
-    if (len < 0) {
-        printf("write data error \n");
+    if(fd < 0) {
+        perror(DEV_NAME);
+        return -1;
     }
     
-    len = read(fd, buf, sizeof(buf));
+    while(1){
+
+        len = read(fd, readBuff, sizeof(readBuff));
         if (len < 0) {
-                printf("read error \n");
-                return -1;
+            printf("read error \n");
+            return -1;
+        }else{
+            printf("%s", writeBuff);
+            len = write(fd, writeBuff, sizeof(writeBuff));
+            if (len < 0) {
+                printf("write data error \n");
+            }
         }
- 
-    printf("%s", buf);
+        
+    }
  
     return(0);
 }
- 
